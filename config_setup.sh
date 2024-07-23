5d0b44f040664692ceb00781beeba9181b60ceee
Thomson Knoles <68712400+thommy890@users.noreply.github.com>
2024-07-23 11:45:46 -0400

    Update config_setup.sh to pull from thommy890/config
    curious to see if it should be /config.git or just /config

--- config_setup.sh
+++ config_setup.sh
@@ -2,7 +2,7 @@ cd ${HOME}
 
 rm ${HOME}/.zshrc
 
-git clone --bare https://github.com/brona90/config.git ${HOME}/.cfg
+git clone --bare https://github.com/thommy890/config.git ${HOME}/.cfg
 
 alias config="/usr/bin/git --git-dir=${HOME}/.cfg/ --work-tree=${HOME}"
 
